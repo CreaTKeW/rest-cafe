93a9d13f23da49184651f405f87241ba943cccda
Damian Ptaszkiewicz <kewinptaszkiewicz@gmail.com>
2025-05-09 01:16:58 +0200

    B
    Add new URL route, update dependencies, and configure project paths
    
    - Updates to requirements.txt.
    - Addition of a new Django URL pattern.
    - Connection of URL paths in the main project configuration.

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('coffee/', include('coffeeshops.urls'))
+    path('coffee/', include('coffeeshops.urls')),
+    path('', include('coffeeshops.urls')),
 ]
